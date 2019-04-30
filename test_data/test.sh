6c07518dda85e99f52966878089f7ac137c5fc44
Lidong <guolidong@genomics.cn>
2019-04-30 10:36:10 +0800

    Update test.sh

--- test_data/test.sh
+++ test_data/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ../filter_barcode.pl  reads.1.fq.gz reads.2.fq.gz barcode.freq 2 2 
-gzip -dc reads.1.fq.gz.filter.gz >tmp.1.filter
-gzip -dc reads.2.fq.gz.filter.gz >tmp.2.filter
+gzip -dc reads.1.fq.gz.filter.fq.gz >tmp.1.filter
+gzip -dc reads.2.fq.gz.filter.fq.gz >tmp.2.filter
 
 diff reads.1.fq.gz.filter tmp.1.filter >diff_cache
 diff reads.2.fq.gz.filter tmp.2.filter >>diff_cache
